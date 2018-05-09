0578e9bf3821d4b5485146e3eca2d52853e9d2a6
XfortunaX <buklin.sergei@yandex.ru>
2018-05-09 21:51:41 +0300

    test

---
+++ server.js
@@ -0,0 +1,16 @@
+const http = require('http');
+const express = require('express');
+const app = express();
+
+app.use(express.static(__dirname + '/'));
+
+app.get(/.*/, function root(req, res) {
+  res.sendFile(__dirname + '/index.html');
+});
+
+const server = http.createServer(app);
+server.listen(process.env.PORT || 3000, function onListen() {
+  const address = server.address();
+  console.log('Listening on: %j', address);
+  console.log(' -> that probably means: http://localhost:%d', address.port);
+});
